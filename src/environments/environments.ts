b4508a99cef036af89996470235c2c11d2ad5f31
Khaled Ayman <164638299+khaledaymn@users.noreply.github.com>
2025-09-10 18:28:58 +0300

    Update environments

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://hrwebsite.runasp.net', // Replace with your backend URL
+  apiUrl: 'https://hrtest.runasp.net', // Replace with your backend URL
   // apiUrl: 'https://shippingmanagementsystem.runasp.net', // Replace with your backend URL
 };
